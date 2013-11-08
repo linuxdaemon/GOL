507cb0baa94e5e1849dd39b4d35d459a2d881dbc
linuxdemon1 <walterbarnes123@gmail.com>
2013-11-08 11:48:36 -0600

    Updated Logc to ignore only the border neighbors instead of ignoring all cells on the border

--- GOL.py
+++ GOL.py
@@ -1,14 +1,49 @@
 #!/usr/bin/python
 import random, os, time, sys
-try:
-	from Tkinter import *
-except ImportError:
-	from Tkinter import *
+#try:
+#	from Tkinter import *
+#except ImportError:
+#	from Tkinter import *
 
-gridhsize = 20
-gridvsize = 20
+gridhsize = 30
+gridvsize = 30
 
-root = Tk()
+#root = Tk()
+
+def isOnCorner(y,x):
+	if (x == gridhsize-1 and y == gridvsize-1) or (x == gridhsize-1 and y == 0) or (x == 0 and y == gridvsize-1) or (x == 0 and y == 0):
+		return True
+	else:
+		return False
+
+def isOnBorder(y,x):
+	if (x == gridhsize-1 or x == 0) and not (y == gridvsize-1 or y == 0):
+		return True
+	elif (y == gridvsize-1 or y == 0) and not (x == gridhsize01 or x == 0):
+		return True
+	else:
+		return False
+
+def onBorder(y,x):
+	if isOnBorder(y,x):
+		if x == gridhsize-1:
+			return "Right"
+		elif x == 0:
+			return "Left"
+		elif y == gridvsize-1:
+			return "Bottom"
+		elif y == 0:
+			return "Top"
+
+def onCorner(y,x):
+	if y == 0 and x == 0:
+		return "TopLeft"
+	elif y == 0 and x == gridhsize-1:
+		return "TopRight"
+	elif y == gridvsize-1 and x == 0:
+		return "BottomLeft"
+	elif y == gridvsize-1 and x == gridhsize-1:
+		return "BottomRight"
 
 def populateGrid():
 	global grid
@@ -50,53 +85,27 @@ def showGrid():
 		rowText = ""
 		for column in range(gridhsize):
 			if grid[column][row] == "true":
-				rowText = rowText+"[+]"
+				rowText += "[@]"
 			elif grid[column][row] == "false":
-				rowText = rowText+"[ ]"
-		print rowText
+				rowText += "[ ]"
+		print(rowText)
 
 def checkNeighbors(column, row):
 	neighbors = 0
-	try:
-		if grid[column][row+1] == "true":
-			neighbors = neighbors+1
-	except:
-		pass
-	try:
-		if grid[column][row-1] == "true":
-			neighbors = neighbors+1
-	except:
-		pass
-	try:
-		if grid[column+1][row] == "true":
-			neighbors = neighbors+1
-	except:
-		pass
-	try:
-		if grid[column-1][row] == "true":
-			neighbors = neighbors+1
-	except:
-		pass
-	try:
-		if grid[column+1][row+1] == "true":
-			neighbors = neighbors+1
-	except:
-		pass
-	try:
-		if grid[column+1][row-1] == "true":
-			neighbors = neighbors+1
-	except:
-		pass
-	try:
-		if grid[column-1][row+1] == "true":
-			neighbors = neighbors+1
-	except:
-		pass
-	try:
-		if grid[column-1][row-1] == "true":
-			neighbors = neighbors+1
-	except:
-		pass
+	if isOnCorner(row,column):
+		corner = True
+		whichCorner = onCorner(row,column)
+	if isOnBorder(row,column):
+		border = True
+		whichBorder = onBorder(row,column)
+	if not (border or corner):
+		for m in [-1,0,1]:
+			for n in [-1,0,1]:
+				if n == 0 and m == 0: pass
+				if grid[column+m][row+n] == "true": neighbors += 1
+	elif border:
+		if whichBorder == "Top":
+			
 	return neighbors
 
 def tickGrid():
@@ -114,7 +123,6 @@ def tickGrid():
 				grid1[column][row] = "true"
 				time.sleep(.001)
 	grid = grid1
-	showGrid()
 	
 def updateGUI():
 	global cellFrame
@@ -128,6 +136,7 @@ def CLI():
 	showGrid()
 	while True:
 		tickGrid()
+		showGrid()
 
 def GUI():
 	populateGrid()
@@ -140,12 +149,12 @@ def GUI():
 	mainloop()
 	updateGUI()
 
-try:
-	if sys.argv[1] == cli or sys.argv[1] == CLI:
-		CLI()
-	elif sys.argv[1] == gui or sys.argv[1] == GUI:
-		GUI()
-except:
-	print "Usage: "+str(sys.argv[0])+" (CLI/cli|GUI/gui)"
-	print sys.argv[1]
-	
+#try:
+#	if sys.argv[1] == cli or sys.argv[1] == CLI:
+#		CLI()
+#	elif sys.argv[1] == gui or sys.argv[1] == GUI:
+#		GUI()
+#except:
+#	print "Usage: "+str(sys.argv[0])+" (CLI/cli|GUI/gui)"
+#	print sys.argv[1]
+CLI()	
