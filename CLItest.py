e29386f88523287c7b398e9af782a9b41b0f3846
linuxdaemon <walter@walterbarnes.net>
2017-06-21 03:37:55 -0500

    Removed old test files

--- CLItest.py
+++
@@ -1,8 +0,0 @@
-from colorama import init
-init()
-from colorama import Fore, Back, Style
-print(Fore.RED + 'some red text')
-print(Back.GREEN + '                  ')
-print(Style.DIM + 'and in dim text')
-print(Fore.RESET + Back.RESET + Style.RESET_ALL)
-print('back to normal now')
